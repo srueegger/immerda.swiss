6b6a6bb7e5157ae0aa7ca8b7d6f7fc7d49a9b5b5
Samuel Rüegger <samuel@rueegger.me>
2021-09-25 18:46:18 +0200

    add option to change page title color

--- wp-content/themes/immerda/index.php
+++ wp-content/themes/immerda/index.php
@@ -3,7 +3,13 @@ get_header();
 if ( have_posts() ) : while ( have_posts() ) : the_post();
   /* Prüfen ob der Titel angezeigt werden soll */
   if( !get_field( 'page_hide_title' ) ) {
-    the_title( '<div class="container header_spacer mb-5"><div class="row"><div class="col-12"><h1 class="text-uppercase">', '</h1></div></div></div>' );
+    /* Prüfen ob der Titel eine spezielle Farbe hat */
+    $page_title_color = get_field( 'page_title_color' );
+    $title_style = '';
+    if( !empty( $page_title_color ) ) {
+      $title_style = ' style="color: ' . $page_title_color . '"';
+    }
+    the_title( '<div class="container header_spacer mb-5"><div class="row"><div class="col-12"><h1 class="text-uppercase"' . $title_style . '>', '</h1></div></div></div>' );
   }
   /* Inhalt ausgeben */
   the_content();
