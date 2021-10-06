97f7c4bc696fc23b2df580e96250319a27171dcf
Samuel Rüegger <samuel@rueegger.me>
2021-10-06 20:29:55 +0200

    Colored Sidebar Buttons

--- wp-content/themes/immerda/header.php
+++ wp-content/themes/immerda/header.php
@@ -26,13 +26,18 @@
   <body <?php body_class(); ?><?php echo $page_background; ?>>
     <div id="main_wrapper">
       <?php
+      /* Sidebar Menü anzeigen, falls eines konfiguriert wurde */
       $scroll_nav_menu_id = get_field( 'page_has_scrollnav', get_queried_object_id() );
       if( !empty( $scroll_nav_menu_id ) ) {
         $scroll_items = wp_get_nav_menu_items( $scroll_nav_menu_id );
         if( !empty( $scroll_items ) ) {
           echo '<ul class="side_btns list-unstyled">';
           foreach( $scroll_items as $item ) {
-            echo '<li><a class="js_scroll_nav" href="' . esc_url( $item->url ) . '">' . esc_attr( $item->title ) . '</a></li>';
+            $color = '';
+            if( get_field( 'menu_color', $item ) ) {
+              $color = ' class="red"';
+            }
+            echo '<li' . $color . '><a class="js_scroll_nav" href="' . esc_url( $item->url ) . '">' . esc_attr( $item->title ) . '</a></li>';
           }
           echo '</ul>';
         }
