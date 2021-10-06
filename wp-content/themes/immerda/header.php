6ec35a4505abac71792b9bc078b417e3d80a08d0
Samuel Rüegger <samuel@rueegger.me>
2021-10-06 20:15:00 +0200

    Firefox für Windows Bugfix

--- wp-content/themes/immerda/header.php
+++ wp-content/themes/immerda/header.php
@@ -3,6 +3,11 @@
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script>
+      paceOptions = {
+        eventLag: false
+      };
+    </script>
     <?php wp_head(); ?>
     <script src="https://kit.fontawesome.com/402771fbad.js" crossorigin="anonymous"></script>
   </head>
